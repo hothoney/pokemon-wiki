a77628706728125a182cd666dac492c0366569fa
Hoon <dt@live.it>
2022-12-20 14:33:45 +0800

    init: 项目初始化

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Pokemon Wiki</title>
+        <meta name="description" content="Pokemon Wiki" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>Hello World</main>
+    </>
+  );
+}
